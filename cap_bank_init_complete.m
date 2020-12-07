475dfe84eb79df81f74bad5df208da92f7c2e8c9
Hanssel <hanssel1301@hotmail.com>
2020-12-07 14:30:19 -0500

    new capacitor bank model

--- cap_bank_init_complete.m
+++
@@ -1,7 +0,0 @@
-function cap_bank=cap_bank_init_complete(mCu,sigmaCu,N)
-    cap_bank=zeros(N,1);
-    for i=N:-1:1
-        cap_bank(N+1-i)=sum(normrnd(mCu,sigmaCu,[(2^(i-1)) 1]));
-    end
-    cap_bank=[cap_bank;normrnd(mCu,sigmaCu)];
-end
